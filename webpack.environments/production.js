cd0b3843b440d53b32cf0125f852ca65918215fb
ali <dandini@gmail.com>
2022-08-02 03:08:46 +0100

    - packages upgrade

---
+++ webpack.environments/production.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const PACKAGE = require('../package.json');
+const version = PACKAGE.version;
+
+module.exports = {
+    mode: "production",
+    entry: {
+        "seatmap.canvas": path.resolve(__dirname, '../src', "lib", "canvas.index.ts")
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    output: {
+        path: path.resolve(__dirname, '../build'),
+        filename: `[name].${version}.js`,
+        clean: true
+    },
+    module: {
+        rules: [
+            {test: /\.tsx?$/, loader: "ts-loader"},
+
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: `[name].${version}.css`,
+            chunkFilename: "[id].css"
+        })
+    ],
+};
