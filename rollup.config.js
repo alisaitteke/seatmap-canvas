d54adef5c97f0bfbd460b5e32182abe20580919f
Ali <alisaitteke@gmail.com>
2023-12-24 17:19:04 +0000

    📦 chore(package.json): update start and build scripts to use rollup instead of webpack for better bundling and build process 🔧 chore(package.json): add rollup plugins for scss, livereload, and serve to enhance development experience 🔧 chore(rollup.config.js): update rollup configuration to use iife format instead of cjs for better compatibility 🔧 chore(rollup.config.js): add rollup plugins for scss, livereload, and serve to enhance development experience

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
+import {terser} from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-
+import serve from 'rollup-plugin-serve'
+import scss from 'rollup-plugin-scss'
+import livereload from 'rollup-plugin-livereload'
 const packageJson = require("./package.json");
 
 export default [
@@ -13,7 +15,7 @@ export default [
         output: [
             {
                 file: packageJson.main,
-                format: "cjs",
+                format: "iife",
                 sourcemap: true,
             },
             {
@@ -23,17 +25,24 @@ export default [
             },
         ],
         plugins: [
+            scss(),
             peerDepsExternal(),
             resolve(),
             commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
+            typescript({tsconfig: "./tsconfig.json"}),
             terser(),
+            livereload(),
+            serve({
+                openPage: '/index.html',
+                contentBase: ['dist', 'examples','src'],
+                port: 3002,
+            })
         ],
         external: [],
     },
     {
         input: "src/lib/canvas.index.ts",
-        output: [{ file: "dist/types.d.ts", format: "es" }],
-        plugins: [dts.default()],
+        output: [{file: "dist/types.d.ts", format: "es"}],
+        plugins: [scss(), dts.default()],
     },
 ];
