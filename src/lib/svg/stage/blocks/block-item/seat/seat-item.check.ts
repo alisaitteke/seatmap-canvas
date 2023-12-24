fea27b7a1fd788fd97e312ce248dbb37fda215e2
Ali <alisaitteke@gmail.com>
2023-12-24 18:55:41 +0000

    🐛 fix(index.html): add a button to randomize blocks and generate random seats 🐛 fix(defaults.model.ts): set default value for check_icon in seat_style 🐛 fix(seat.model.ts): add icon property to SeatModel class 🐛 fix(seat-item.check.ts): change tag from "text" to "path" and update attributes to display check icon correctly

--- src/lib/svg/stage/blocks/block-item/seat/seat-item.check.ts
+++ src/lib/svg/stage/blocks/block-item/seat/seat-item.check.ts
@@ -1,6 +1,6 @@
 /*
  * $project.fileName
- * https://github.com/alisaitteke/seatmap-canvas Copyright 2023 Ali Sait TEKE
+ * https://github.com/seatmap/canvas Copyright 2018 Ali Sait TEKE
  */
 
 import SvgBase from "../../../../svg.base";
@@ -8,7 +8,7 @@ import {dom} from "../../../../../decorators/dom";
 import {SeatItem} from "./seat-item.index";
 
 @dom({
-    tag: "text",
+    tag: "path",
     class: "seat-check",
     autoGenerate: false
 })
@@ -16,16 +16,11 @@ export class SeatItemCheck extends SvgBase {
 
     constructor(public parent: SeatItem) {
         super(parent);
-        // this.attr("d", "M12.9,953.7l-6.3,6.5l-2.9-2.5l-2.1,2.4l4.1,3.5l1.1,1l1.1-1.1l7.3-7.6L12.9,953.7L12.9,953.7z");
-        this.text('\uf005')
-        this.attr("class", 'fa');
-        // this.attr("fill", '#ff0000');
-
-        this.attr("fill", this.global.config.seat_style.check_icon_color);
-        this.attr("transform", "translate(-7,4) scale(0.8)");
+        this.attr("d", "M12.9,953.7l-6.3,6.5l-2.9-2.5l-2.1,2.4l4.1,3.5l1.1,1l1.1-1.1l7.3-7.6L12.9,953.7L12.9,953.7z");
+        this.attr("fill", this.global.config.seat_style.check_color);
+        this.attr("transform", "translate(-8,-959.36218)");
         this.attr("pointer-events", "none");
         this.attr("display", "none");
-        this.attr("opacity", 0);
 
         return this;
     }
@@ -40,13 +35,11 @@ export class SeatItemCheck extends SvgBase {
 
     show():this {
         this.node.attr("display", "block");
-        this.node.attr("opacity", 1);
         return this;
     }
 
     hide():this {
         this.node.attr("display", "none");
-        this.node.attr("opacity", 0);
         return this;
     }
 }
