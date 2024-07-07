c3c1bea84aaf72a98a778e689fe700ba8a6a5542
John <john@allmyfriends.work>
2024-07-07 18:57:26 +0100

    refactor(models): update import paths to use absolute paths for better clarity and maintainability

--- src/lib/models/global.model.ts
+++ src/lib/models/global.model.ts
@@ -1,15 +1,14 @@
 /*
- * $project.fileName
  * https://github.com/alisaitteke/seatmap-canvas Copyright 2023 Ali Sait TEKE
  */
 
-import DefaultsModel from "./defaults.model";
-import DataModel from "./data.model";
-import {SeatMapCanvas} from "../canvas.index";
-import ZoomManager from "../svg/zoom.manager";
-import EventManager from "../svg/event.manager";
-import Svg from "../svg/svg.index";
-import WindowManager from "../window.manager";
+import DefaultsModel from "@model/defaults.model";
+import DataModel from "@model/data.model";
+import {SeatMapCanvas} from "@/canvas.index";
+import ZoomManager from "@svg/zoom.manager";
+import EventManager from "@svg/event.manager";
+import Svg from "@svg/svg.index";
+import WindowManager from "@/window.manager";
 
 export interface GlobalModel {
     config: DefaultsModel,
