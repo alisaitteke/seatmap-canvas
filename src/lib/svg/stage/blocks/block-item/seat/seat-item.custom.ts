a446e5dd95a54d8d04506b2399d771fcda81484e
Ali Sait Teke <alisaitteke@gmail.com>
2025-01-29 03:13:53 +0000

    feat(svg): add new seat SVG asset and custom seat item classes
    Add seat-1.svg to provide a visual representation of a seat.
    Introduce SeatItemCheck and SeatItemCircle classes to enhance
    customization and interaction with seat elements in the SVG stage.

---
+++ src/lib/svg/stage/blocks/block-item/seat/seat-item.custom.ts
@@ -0,0 +1,31 @@
+/*
+ * $project.fileName
+ * https://github.com/alisaitteke/seatmap-canvas Copyright 2023 Ali Sait TEKE
+ */
+
+import SvgBase from "@svg/svg.base";
+
+import {dom} from "@decorator/dom";
+import {SeatItem} from "./seat-item.index";
+
+@dom({
+    tag: "circle",
+    class: "seat-circle",
+    autoGenerate: false
+})
+export class SeatItemCircle extends SvgBase {
+
+    constructor(public parent: SeatItem) {
+        super(parent);
+        this.attr("block-id", parent.item.block.id);
+        this.attr("r", this.global.config.style.seat.radius);
+        this.attr("fill", this.global.config.style.seat.color);
+        this.attr("stroke", "rgba(0,0,0,0.3)");
+        this.attr("stroke-width", 1);
+        return this;
+    }
+
+    update(): this {
+        return this;
+    }
+}
