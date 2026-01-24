ec1261674f0eff1cdc826aa94a807e588fae1d36
Ali Sait Teke <alisaitteke@gmail.com>
2026-01-24 19:30:10 +0000

    fix(index.tsx): update link for "Get Started" button to point to the correct installation page for better user guidance

--- docs-site/src/pages/index.tsx
+++ docs-site/src/pages/index.tsx
@@ -36,7 +36,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
-            to="/docs/intro">
+            to="/docs/getting-started/installation">
             Get Started 🚀
           </Link>
           <Link
